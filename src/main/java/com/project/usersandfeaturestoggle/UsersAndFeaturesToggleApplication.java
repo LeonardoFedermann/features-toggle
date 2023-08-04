858a066a91c5855e458fbe7caed1a8948eb43693
lfedermaext_elcomp <lfedermaext@estee.com>
2023-08-04 17:22:33 -0300

    created spring project

---
+++ src/main/java/com/project/usersandfeaturestoggle/UsersAndFeaturesToggleApplication.java
@@ -0,0 +1,11 @@
+package com.project.usersandfeaturestoggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersAndFeaturesToggleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UsersAndFeaturesToggleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/project/usersandfeaturestoggle/UsersAndFeaturesToggleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.usersandfeaturestoggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersAndFeaturesToggleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
