ef5e417032ea4367fca3aaf84fb2e0fd882a5d47
lfedermaext_elcomp <lfedermaext@estee.com>
2023-08-09 18:58:39 -0300

    creating basic dockerfile

--- src/main/java/com/project/usersandfeaturestoggle/UsersAndFeaturesToggleApplication.java
+++ src/main/java/com/project/usersandfeaturestoggle/UsersAndFeaturesToggleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UsersAndFeaturesToggleApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(UsersAndFeaturesToggleApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(UsersAndFeaturesToggleApplication.class, args);
+	}
 }
